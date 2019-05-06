c476babf5c7293c825f16ee170e45e0a51596b02
ErickRc <erick@spaceshiplabs.com>
2019-05-06 11:39:43 -0500

    environmental vars config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
-require("dotenv").config({
-  path: `${process.env.NODE_ENV}.env`,
-})
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({
+    path: `${process.env.NODE_ENV}.env`,
+  })
+}
 
 module.exports = {
   pathPrefix: "/gatsbyPrismic",
