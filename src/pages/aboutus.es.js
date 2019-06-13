14a208414d6f8b9dd54b1ef94e06054a871b52d7
Luis <luis19prz@gmail.com>
2019-06-13 12:11:34 -0500

    about us page

---
+++ src/pages/aboutus.es.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react"
+import { graphql } from "gatsby"
+import {
+  Section,
+  Container,
+  Subtitle,
+  HtmlContent,
+} from "../theme/index.styled"
+import SEO from "../components/seo"
+import BannerComponent from "../components/homebanner/index"
+import Layout from "../components/layout"
+import "moment/locale/es"
+import moment from "moment"
+
+import { Context, ContextEs } from "../languages/context"
+
+moment.locale("es")
+
+class SampleEsPage extends Component {
+  render() {
+    const data = this.props.data.prismicLandingPages.data
+    return (
+      <Context.Provider value={ContextEs}>
+        <Layout langKey="es">
+          <SEO title={data.title.text} keywords={[`Border Center`]} />
+
+          <BannerComponent
+            data={{
+              cover: data.cover,
+              title: data.title,
+            }}
+            fullHeight={false}
+          />
+          <Section>
+            <Container>
+              <Subtitle>{data.subtitle.text}</Subtitle>
+              <HtmlContent
+                dangerouslySetInnerHTML={{ __html: data.content.html }}
+              />
+            </Container>
+          </Section>
+        </Layout>
+      </Context.Provider>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query AboutUsEsPageQuery {
+    prismicLandingPages(uid: { eq: "quienes-somos" }, lang: { eq: "es-mx" }) {
+      ...landingPageCompleteDataFragment
+    }
+  }
+`
+
+export default SampleEsPage
